3221c2c7f2c7c25ce5510238eda8f601ffeb7f33
Wilko Manger <wilko.manger@wearespindle.com>
2021-07-28 10:16:39 +0200

    Queue method channel invocations on Android

--- android/src/main/kotlin/org/openvoipalliance/flutterphonelib/FlutterCallback.kt
+++ android/src/main/kotlin/org/openvoipalliance/flutterphonelib/FlutterCallback.kt
@@ -11,9 +11,6 @@ import io.flutter.view.FlutterCallbackInformation
 import io.flutter.view.FlutterMain
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.atomic.AtomicBoolean
-import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
@@ -22,11 +19,13 @@ private object FlutterCallback {
     private lateinit var flutterEngine: FlutterEngine
     private lateinit var methodChannel: MethodChannel
 
+    private val jobExecutor = QueuedJobExecutor()
+
     fun register(context: Context, key: String, handle: Long) {
         context.sharedPreferences
-                .edit()
-                .putLong(key, handle)
-                .apply()
+            .edit()
+            .putLong(key, handle)
+            .apply()
     }
 
     private fun handleOf(context: Context, key: String): Long {
@@ -55,68 +54,117 @@ private object FlutterCallback {
 
             flutterEngine = FlutterEngine(context)
             methodChannel = MethodChannel(
-                    flutterEngine.dartExecutor.binaryMessenger,
-                    "org.openvoipalliance.flutterphonelib/background"
+                flutterEngine.dartExecutor.binaryMessenger,
+                "org.openvoipalliance.flutterphonelib/background"
             )
         }
 
         GlobalScope.launch {
-            if (!flutterEngine.dartExecutor.isExecutingDart) {
-                // We wait until the callback dispatcher isolate is initialized on the Dart side
-                // and we can send methods.
-                suspendCoroutine<Unit> { continuation ->
-                    // This needs to happen on the main thread.
-                    launch(Dispatchers.Main) {
-                        val callbackHandle = handleOf(context, PhoneLib.Keys.CALLBACK_DISPATCHER)
-                        val callbackInfo = FlutterCallbackInformation
-                                .lookupCallbackInformation(callbackHandle)
+            if (!jobExecutor.isRunning) {
+                jobExecutor.start()
+            }
 
-                        val callback = DartExecutor.DartCallback(
-                                context.assets,
-                                FlutterMain.findAppBundlePath(),
-                                callbackInfo
-                        )
+            jobExecutor.add(
+                // Job has to be lazy, since it should be started by the executor and
+                // not immediately.
+                GlobalScope.launch(start = CoroutineStart.LAZY) {
+                    if (!flutterEngine.dartExecutor.isExecutingDart) {
+                        // We wait until the callback dispatcher isolate is initialized on the Dart side
+                        // and we can send methods.
+                        suspendCoroutine<Unit> { continuation ->
+                            // This needs to happen on the main thread.
+                            launch(Dispatchers.Main) {
+                                val callbackHandle =
+                                    handleOf(context, PhoneLib.Keys.CALLBACK_DISPATCHER)
+                                val callbackInfo = FlutterCallbackInformation
+                                    .lookupCallbackInformation(callbackHandle)
 
-                        methodChannel.setMethodCallHandler { call, result ->
-                            if (call.method == "callbackDispatcherIsInitialized") {
-                                Log.d(
-                                        PhoneLib.LOG_TAG,
-                                        "Callback dispatcher is ready to receive methods"
+                                val callback = DartExecutor.DartCallback(
+                                    context.assets,
+                                    FlutterMain.findAppBundlePath(),
+                                    callbackInfo
                                 )
-                                continuation.resume(Unit)
-                                result.success(null)
+
+                                methodChannel.setMethodCallHandler { call, result ->
+                                    if (call.method == "callbackDispatcherIsInitialized") {
+                                        Log.d(
+                                            PhoneLib.LOG_TAG,
+                                            "Callback dispatcher is ready to receive methods"
+                                        )
+                                        continuation.resume(Unit)
+                                        result.success(null)
+                                    }
+                                }
+
+                                Log.d(PhoneLib.LOG_TAG, "Executing callback dispatcher")
+                                flutterEngine.dartExecutor.executeDartCallback(callback)
+                                Log.d(
+                                    PhoneLib.LOG_TAG,
+                                    "Waiting until callback dispatcher is ready to receive method calls"
+                                )
                             }
                         }
+                    }
 
-                        Log.d(PhoneLib.LOG_TAG, "Executing callback dispatcher")
-                        flutterEngine.dartExecutor.executeDartCallback(callback)
+                    val initialize = handleOf(context, PhoneLib.Keys.INITIALIZE)
+                    val methodCallbackHandle = handleOf(context, method)
+
+                    Handler(Looper.getMainLooper()).post {
                         Log.d(
-                                PhoneLib.LOG_TAG,
-                                "Waiting until callback dispatcher is ready to receive method calls"
+                            PhoneLib.LOG_TAG,
+                            "Invoking method through callback dispatcher: $method"
+                        )
+                        methodChannel.invokeMethod(
+                            method,
+                            listOf(initialize, methodCallbackHandle, *arguments)
                         )
                     }
                 }
-            }
-
-            val initialize = handleOf(context, PhoneLib.Keys.INITIALIZE)
-            val methodCallbackHandle = handleOf(context, method)
-
-            Handler(Looper.getMainLooper()).post {
-                Log.d(
-                        PhoneLib.LOG_TAG,
-                        "Invoking method through callback dispatcher: $method"
-                )
-                methodChannel.invokeMethod(
-                        method,
-                        listOf(initialize, methodCallbackHandle, *arguments)
-                )
-            }
+            )
         }
     }
 }
 
 fun Context.registerFlutterCallback(key: String, handle: Long) =
-        FlutterCallback.register(this, key, handle)
+    FlutterCallback.register(this, key, handle)
 
 fun Context.invokeMethodThroughCallback(method: String, vararg arguments: Any?) =
-        FlutterCallback.invokeMethodThroughCallback(this@invokeMethodThroughCallback, method, *arguments)
+    FlutterCallback.invokeMethodThroughCallback(
+        this@invokeMethodThroughCallback,
+        method,
+        *arguments
+    )
+
+private class QueuedJobExecutor {
+    private val channel = Channel<Job>()
+
+    var isRunning = false
+        private set
+
+    suspend fun add(job: Job) {
+        if (job.isActive) {
+            Log.e(
+                PhoneLib.LOG_TAG,
+                "Job added to queue is already active, this should never happen!"
+            )
+            return
+        }
+
+        channel.send(job)
+    }
+
+    fun start() {
+        isRunning = true
+
+        // We don't want to block, so run jobs in a coroutine.
+        GlobalScope.launch {
+            while (isRunning) {
+                channel.receive().join()
+            }
+        }
+    }
+
+    fun stop() {
+        isRunning = false
+    }
+}
