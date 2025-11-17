ac8ac4e28b1c303ca23b5cc0fd37a926af2acfe1
Jeremy Norman <jeremynorman89@gmail.com>
2025-11-17 12:42:40 +0000

    chore(deps): bump APL to 0.1.143 and IPL to 0.1.16
    Update native library dependencies to include DTMF functionality:
    - Android Phone Integration Lib: 0.1.142 -> 0.1.143
    - iOS Phone Lib: 0.1.15 -> 0.1.16
    
    #MCONV-666

--- ios/flutter_phone_lib/Package.swift
+++ ios/flutter_phone_lib/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/open-voip-alliance/iOS-Phone-Lib.git", exact: "0.1.15"),
+        .package(url: "https://github.com/open-voip-alliance/iOS-Phone-Lib.git", exact: "0.1.16"),
     ],
     targets: [
         .target(
